6f7c73ff820d10ebaa959e51f20a17f9f431c956
Man Vu <33613479+man-vu@users.noreply.github.com>
2025-06-17 13:03:24 -0400

    Alias fields in CorrectAnswerModel

--- server/new_models/CorrectAnswerModel.ts
+++ server/new_models/CorrectAnswerModel.ts
@@ -2,11 +2,16 @@ import prisma from '../prismaClient';
 
 export class CorrectAnswerModel {
   static findAll(quizId: number) {
-    const query = `SELECT q.QuestionId, qmc.ChoiceText, qmc.ChoiceOrder as choice_id,
-      qmc.IsCorrect as is_correct_choice, qgf.SequenceId, qgf.CorrectAnswer,
-      mp.PromptOrder as prompt_order, mc.ChoiceOrder as choice_order,
-      CASE WHEN ma.ChoiceId IS NULL THEN 0 ELSE 1 END as is_correct,
-      q.TypeId
+    const query = `SELECT q.QuestionId as question_id,
+      q.TypeId as type_id,
+      qmc.ChoiceText as choice_text,
+      qmc.ChoiceOrder as choice_id,
+      qmc.IsCorrect as is_correct_choice,
+      qgf.SequenceId as sequence_id,
+      qgf.CorrectAnswer as correct_answer,
+      mp.PromptOrder as prompt_order,
+      mc.ChoiceOrder as choice_order,
+      CASE WHEN ma.ChoiceId IS NULL THEN 0 ELSE 1 END as is_correct
       FROM Question q
       JOIN QuizQuestion qq ON qq.QuestionId = q.QuestionId
       LEFT JOIN QuestionMultipleChoice qmc ON q.QuestionId = qmc.QuestionId
