c09bd1ebf17f14ff6216e949a9281945720958af
Man Vu <33613479+man-vu@users.noreply.github.com>
2025-06-17 11:31:34 -0400

    fix correct answer query for new table names

--- server/new_models/CorrectAnswerModel.ts
+++ server/new_models/CorrectAnswerModel.ts
@@ -2,16 +2,16 @@ import prisma from '../prismaClient';
 
 export class CorrectAnswerModel {
   static findAll(quizId: number) {
-    const query = `SELECT q.question_id, qmc.choice_text, qmc.choice_order as choice_id, qmc.is_correct as is_correct_choice,
-      qgf.sequence_id, qgf.correct_answer, qmp.correct_answers, q.type_id
-      FROM question q
-      JOIN quiz_question qq ON qq.question_id = q.question_id
-      JOIN question_instruction qi ON q.instruction_id = qi.instruction_id
-      LEFT JOIN question_multiple_choice qmc ON q.question_id =  qmc.question_id
-      LEFT JOIN question_gap_filling qgf ON q.question_id = qgf.question_id
-      LEFT JOIN question_matching qmp ON q.question_id = qmp.question_id
-      WHERE qq.quiz_id = ${quizId} AND q.is_active = 1
-      ORDER BY q.question_id, qmc.choice_order, qgf.sequence_id`;
+    const query = `SELECT q.QuestionId, qmc.ChoiceText, qmc.ChoiceOrder as choice_id, qmc.IsCorrect as is_correct_choice,
+      qgf.SequenceId, qgf.CorrectAnswer, qmp.CorrectAnswers, q.TypeId
+      FROM Question q
+      JOIN QuizQuestion qq ON qq.QuestionId = q.QuestionId
+      JOIN QuestionInstruction qi ON q.InstructionId = qi.InstructionId
+      LEFT JOIN QuestionMultipleChoice qmc ON q.QuestionId = qmc.QuestionId
+      LEFT JOIN QuestionGapFilling qgf ON q.QuestionId = qgf.QuestionId
+      LEFT JOIN QuestionMatchingPair qmp ON q.QuestionId = qmp.QuestionId
+      WHERE qq.QuizId = ${quizId} AND q.IsActive = 1
+      ORDER BY q.QuestionId, qmc.ChoiceOrder, qgf.SequenceId`;
     return prisma.$queryRawUnsafe(query);
   }
 }
