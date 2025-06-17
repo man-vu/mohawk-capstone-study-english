0356f2dda36c3cb7aa1f7e4e7433480946822fac
Man Vu <33613479+man-vu@users.noreply.github.com>
2025-06-17 00:28:51 -0400

    Use type-only Prisma imports

--- server/new_models/UserFavoriteModel.ts
+++ server/new_models/UserFavoriteModel.ts
@@ -1,5 +1,5 @@
 import prisma from '../prismaClient';
-import { Prisma } from '@prisma/client';
+import type { Prisma } from '@prisma/client';
 export interface UserFavorite {
   UserId: number;
   QuizId: number;
