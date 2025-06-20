d1cc4ba2172dc566a2435b0e97289a46e1ba307e
Man Vu <33613479+man-vu@users.noreply.github.com>
2025-06-20 00:05:33 -0400

    Remove aws-sdk dependency and localize avatar uploads

--- server/config/index.ts
+++ server/config/index.ts
@@ -30,9 +30,6 @@ const envFile = {
   datetime_format: process.env.DATETIME_FORMAT,
   appmail: process.env.APPMAIL,
   appmail_password: process.env.APPMAIL_PASSWORD,
-  aws_access_key: process.env.AWS_ACCESS_KEY,
-  aws_secret_key: process.env.AWS_SECRET_KEY,
-  s3_bucket_name: process.env.S3_BUCKET_NAME,
 };
 export const {
   vue_app_server_endpoint,
@@ -52,9 +49,6 @@ export const {
   datetime_format,
   appmail,
   appmail_password,
-  aws_access_key,
-  aws_secret_key,
-  s3_bucket_name,
 } = envFile;
 
 export default envFile;
